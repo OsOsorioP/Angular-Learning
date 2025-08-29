1ca0ac557913e42c6e3147733edafef961df0b6e
workspace014-admin <oscar.osorio1884@gmail.com>
2025-08-29 13:38:42 -0500

    feat: add routing for Hero and Counter pages in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { CounterPageComponent } from './pages/counter/counter-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'hero',
+    component: HeroPageComponent
+  }
+];
