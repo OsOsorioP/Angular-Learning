4832709d61c023ea9134041f769981db3b04de6b
workspace014-admin <oscar.osorio1884@gmail.com>
2025-08-30 07:30:27 -0500

    feat: add computed properties for hero description and capitalized name in HeroPageComponent

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,11 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './hero-page.component.html',
+  imports: [UpperCasePipe]
 })
 export class HeroPageComponent {
   name = signal('Ironman');
   age = signal(45);
+
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  });
+
+  capitalizedName = computed(() => {
+    this.name().toUpperCase();
+  });
 
   getHeroDescription() {
     return `${this.name()} - ${this.age()}`;
